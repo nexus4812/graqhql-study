52ff16c1baa5aaf1e2cb72549eefd869b9c9e4dd
nexus4812 <powerbatteryboy@gmail.com>
2020-12-20 12:32:10 +0900

    Add env

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUBTOKEN;
+console.log(GITHUB_TOKEN);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div>
-          hi
+            hi
         </div>
       </header>
     </div>
