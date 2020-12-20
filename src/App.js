57cd47f161617cc6c398958207ece74a1ca04e59
nexus4812 <powerbatteryboy@gmail.com>
2020-12-20 20:58:49 +0900

    Add query input

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import client from './client';
 import {Query} from "react-apollo";
 import {SEARCH_REPOSITORIES} from "./graqhql";
 
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 const DEFAULT_STATE = {
     first: 5,
@@ -18,27 +18,32 @@ function App() {
 
     const {first, after, last, before, query} = state;
 
+    const handleChange = (event) => {
+        setState({
+                ...state,
+                query: event.target.value
+            }
+        );
+        console.log(event.target.value);
+    };
+
     return (
         <div className="App">
             <header className="App-header">
 
                 <ApolloProvider client={client}>
-                    <div>
-                        hi
-                    </div>
+                    <form action="">
+                        <input type="text" value={state.query} onChange={handleChange}/>
+                    </form>
 
                     <Query
-                        query= {SEARCH_REPOSITORIES}
-                        variables= {{first, after, last, before, query}}
+                        query={SEARCH_REPOSITORIES}
+                        variables={{first, after, last, before, query}}
                     >
                         {
                             ({loading, error, data}) => {
-                                console.log(loading);
-
                                 if (loading) return 'Loading...';
                                 if (error) return error.message;
-
-
                                 console.log(data);
                                 return ''
                             }
